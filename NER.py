98a7af025b75499863e94a15d140d9203ebde625
sefinehtesfa <sefinehtesfa@gmail.com>
2024-05-24 21:27:11 +0300

    Model training

--- NER.py
+++ NER.py
@@ -65,8 +65,8 @@ def compute_metrics(p):
 # Training arguments
 training_args = TrainingArguments(
     output_dir="./results",
-    eval_strategy="epoch",
-    learning_rate=0.01,
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
     num_train_epochs=3,
